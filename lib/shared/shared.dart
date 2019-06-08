c8f7be074364dcc1bbfebd67824d22dc38100036
Niklas Sauter <niki02.sauter@gmx.de>
2019-06-08 21:35:08 +0200

    add custom_bottom_sheet.dart

--- lib/shared/shared.dart
+++ lib/shared/shared.dart
@@ -1,5 +1,6 @@
 export 'connection_dialog.dart';
 export 'connection_widget_tile.dart';
+export 'custom_bottom_sheet.dart';
 export 'custom_icon_button.dart';
 export 'custom_show_dialog.dart';
 export 'custom_theme.dart';
