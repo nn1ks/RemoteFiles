46de886067fb3679246bf8c54d330b8203e0e55a
Niklas Sauter <niki02.sauter@gmx.de>
2019-10-10 16:47:14 +0200

    update about.dart

--- lib/pages/about.dart
+++ lib/pages/about.dart
@@ -68,10 +68,24 @@ class _AboutPageState extends State<AboutPage> {
     }
   }
 
-  Future<void> _launchPlayStoreUrl() async {
-    String url =
-        "https://play.google.com/store/apps/details?id=com.niklas8.remotefiles";
-    await _launchUrl(url);
+  Widget _buildButton(
+    BuildContext context, {
+    @required IconData iconData,
+    @required String label,
+    @required String url,
+  }) {
+    return RaisedButton.icon(
+      color: Provider.of<CustomTheme>(context).isLightTheme(context)
+          ? Colors.grey[200]
+          : Colors.grey[850],
+      elevation: 0,
+      highlightElevation: 4,
+      icon: Icon(iconData),
+      label: Text(label),
+      onPressed: () async {
+        await _launchUrl(url);
+      },
+    );
   }
 
   @override
@@ -124,138 +138,101 @@ class _AboutPageState extends State<AboutPage> {
           ),
           SizedBox(height: 5),
           Text(
-            _version,
+            _version ?? "Version unknown",
             textAlign: TextAlign.center,
             style: TextStyle(
               fontSize: 16,
               color: Theme.of(context).hintColor,
             ),
           ),
-          SizedBox(height: 5),
-          Container(
-            margin: EdgeInsets.all(16),
-            padding: EdgeInsets.symmetric(vertical: 8),
-            decoration: BoxDecoration(
-              border: Border.all(
-                width: 1,
-                color: Theme.of(context).dividerColor,
-              ),
-              borderRadius: BorderRadius.circular(8),
-            ),
-            child: Column(
+          SizedBox(height: 14),
+          Padding(
+            padding: EdgeInsets.all(10),
+            child: Wrap(
+              alignment: WrapAlignment.center,
+              spacing: 14,
               children: <Widget>[
-                ListTile(
-                  leading: Icon(MdiIcons.githubCircle),
-                  title: Text("GitHub"),
-                  onTap: () async {
-                    await _launchUrl("https://github.com/niklas-8/RemoteFiles");
-                  },
+                _buildButton(
+                  context,
+                  iconData: MdiIcons.githubCircle,
+                  label: "GitHub",
+                  url: "https://github.com/niklas-8/RemoteFiles",
                 ),
-                ListTile(
-                  leading: Icon(MdiIcons.googlePlay),
-                  title: Text("Google PlayStore"),
-                  onTap: () async {
-                    await _launchPlayStoreUrl();
-                  },
+                _buildButton(
+                  context,
+                  iconData: Icons.link,
+                  label: "Website",
+                  url: "https://niklas-8.github.io/RemoteFiles",
                 ),
-                ListTile(
-                  leading: Icon(Icons.link),
-                  title: Text("Website"),
-                  onTap: () async {
-                    await _launchUrl("https://niklas-8.github.io/RemoteFiles");
-                  },
+                _buildButton(
+                  context,
+                  iconData: MdiIcons.googlePlay,
+                  label: "Google PlayStore",
+                  url:
+                      "https://play.google.com/store/apps/details?id=com.niklas8.remotefiles",
                 ),
               ],
             ),
           ),
-          Container(
-            margin: EdgeInsets.symmetric(horizontal: 16, vertical: 4),
-            padding: EdgeInsets.symmetric(vertical: 8),
-            decoration: BoxDecoration(
-              border: Border.all(
-                width: 1,
-                color: Theme.of(context).dividerColor,
-              ),
-              borderRadius: BorderRadius.circular(8),
-            ),
-            child: Column(
-              children: <Widget>[
-                ListTile(
-                  leading: _isLatestVersion == null
-                      ? SizedBox(
-                          height: 22,
-                          width: 22,
-                          child: CircularProgressIndicator(
-                            strokeWidth: 2.4,
-                            valueColor: AlwaysStoppedAnimation(
-                              Provider.of<CustomTheme>(context)
-                                      .isLightTheme(context)
-                                  ? Colors.black45
-                                  : Theme.of(context).iconTheme.color,
-                            ),
+          Divider(),
+          Column(
+            children: <Widget>[
+              ListTile(
+                leading: _isLatestVersion == null
+                    ? SizedBox(
+                        height: 22,
+                        width: 22,
+                        child: CircularProgressIndicator(
+                          strokeWidth: 2.4,
+                          valueColor: AlwaysStoppedAnimation(
+                            Provider.of<CustomTheme>(context)
+                                    .isLightTheme(context)
+                                ? Colors.black45
+                                : Theme.of(context).iconTheme.color,
                           ),
-                        )
-                      : Icon(
-                          _isLatestVersion ? Icons.done : Icons.error_outline),
-                  title: Text(
-                    _isLatestVersion == null
-                        ? "Checking for updates..."
-                        : (_isLatestVersion
-                            ? "You have the latest version"
-                            : "You don't have the latest version"),
-                  ),
+                        ),
+                      )
+                    : Icon(_isLatestVersion ? Icons.done : Icons.error_outline),
+                title: Text(
+                  _isLatestVersion == null
+                      ? "Checking for updates..."
+                      : (_isLatestVersion
+                          ? "You have the latest version"
+                          : "You don't have the latest version"),
                 ),
-                _isLatestVersion == null
-                    ? Container()
-                    : (_isLatestVersion
-                        ? Container()
-                        : Column(
-                            mainAxisSize: MainAxisSize.min,
-                            children: <Widget>[
-                              Container(
-                                margin: EdgeInsets.symmetric(vertical: 6),
-                                height: 1,
-                                color: Theme.of(context).dividerColor,
-                              ),
-                              ListTile(
-                                leading: Icon(Icons.open_in_new),
-                                title: Text("Update on PlayStore"),
-                                onTap: () async {
-                                  await _launchPlayStoreUrl();
-                                },
-                              ),
-                              ListTile(
-                                leading: Icon(Icons.save_alt),
-                                title: Text(
-                                    "Download the latest version from GitHub"),
-                                onTap: () async {
-                                  await _launchUrl(_latestVersion["assets"][0]
-                                      ["browser_download_url"]);
-                                },
-                              ),
-                            ],
-                          )),
-              ],
-            ),
-          ),
-          Container(
-            margin: EdgeInsets.all(16),
-            padding: EdgeInsets.symmetric(vertical: 8),
-            decoration: BoxDecoration(
-              border: Border.all(
-                width: 1,
-                color: Theme.of(context).dividerColor,
               ),
-              borderRadius: BorderRadius.circular(8),
-            ),
-            child: Column(
-              children: <Widget>[
-                ListTile(
-                  title: Text("If you like the app please consider rating it " +
-                      "on the Google PlayStore or donating via GitHub"),
-                ),
-              ],
-            ),
+              if (_isLatestVersion != null)
+                if (!_isLatestVersion)
+                  Padding(
+                    padding: EdgeInsets.all(10),
+                    child: Wrap(
+                      alignment: WrapAlignment.center,
+                      spacing: 14,
+                      children: <Widget>[
+                        _buildButton(
+                          context,
+                          iconData: Icons.open_in_new,
+                          label: "Update on PlayStore",
+                          url:
+                              "https://play.google.com/store/apps/details?id=com.niklas8.remotefiles",
+                        ),
+                        _buildButton(
+                          context,
+                          iconData: Icons.save_alt,
+                          label: "Download from GitHub",
+                          url: _latestVersion["assets"][0]
+                              ["browser_download_url"],
+                        ),
+                      ],
+                    ),
+                  ),
+            ],
+          ),
+          Divider(),
+          ListTile(
+            leading: Icon(MdiIcons.heartOutline),
+            title: Text("If you like the app please consider rating it " +
+                "on the Google PlayStore or donating via GitHub"),
           ),
         ],
       ),
